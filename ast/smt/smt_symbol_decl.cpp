e841f60f3b67e01d5e4b40f3de7e9fbf6e26f7af
Cristina Serban <cristina.serban89@gmail.com>
2015-04-20 16:40:41 +0200

    Documentation and implementation of symbol declarations

--- ast/smt/smt_symbol_decl.cpp
+++ ast/smt/smt_symbol_decl.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 using namespace smt;
 
+/* =============================== SortSymbolDeclaration ============================== */
+
 SortSymbolDeclaration::SortSymbolDeclaration(shared_ptr<IIdentifier> identifier,
                                              long cardinality) {
     setIdentifier(identifier);
@@ -20,11 +22,11 @@ SortSymbolDeclaration::SortSymbolDeclaration(shared_ptr<IIdentifier> identifier,
     setCardinality(cardinality);
 
     for(vector<shared_ptr<Attribute>>::iterator it = attributes.begin(); it != attributes.end(); it++) {
-        attributes.push_back(*it);
+        this->attributes.push_back(*it);
     }
 }
 
-shared_ptr<IIdentifier> &SortSymbolDeclaration::getIdentifier() {
+shared_ptr<IIdentifier> SortSymbolDeclaration::getIdentifier() {
     return identifier;
 }
 
@@ -42,4 +44,162 @@ void SortSymbolDeclaration::setCardinality(long cardinality) {
 
 vector<shared_ptr<Attribute>> &SortSymbolDeclaration::getAttributes() {
     return attributes;
+}
+
+/* ============================= SpecConstFunDeclaration ============================== */
+
+SpecConstFunDeclaration::SpecConstFunDeclaration(shared_ptr<ISpecConstant> constant,
+                                                 shared_ptr<Sort> sort) {
+    setConstant(constant);
+    setSort(sort);
+}
+
+SpecConstFunDeclaration::SpecConstFunDeclaration(shared_ptr<ISpecConstant> constant,
+                                                 shared_ptr<Sort> sort,
+                                                 vector<shared_ptr<Attribute>> &attributes) {
+    setConstant(constant);
+    setSort(sort);
+
+    for(vector<shared_ptr<Attribute>>::iterator it = attributes.begin(); it != attributes.end(); it++) {
+        this->attributes.push_back(*it);
+    }
+}
+
+shared_ptr<ISpecConstant> SpecConstFunDeclaration::getConstant() {
+    return constant;
+}
+
+void SpecConstFunDeclaration::setConstant(shared_ptr<ISpecConstant> constant) {
+    this->constant = constant;
+}
+
+shared_ptr<Sort> SpecConstFunDeclaration::getSort() {
+    return sort;
+}
+
+void SpecConstFunDeclaration::setSort(shared_ptr<Sort> sort) {
+    this->sort = sort;
+}
+
+vector<shared_ptr<Attribute>> &SpecConstFunDeclaration::getAttributes() {
+    return attributes;
+}
+
+/* ========================== MetaSpecConstFunDeclaration =========================== */
+
+MetaSpecConstFunDeclaration::MetaSpecConstFunDeclaration(shared_ptr<MetaSpecConstant> constant,
+                                                         shared_ptr<Sort> sort) {
+    setConstant(constant);
+    setSort(sort);
+}
+
+MetaSpecConstFunDeclaration::MetaSpecConstFunDeclaration(shared_ptr<MetaSpecConstant> constant,
+                                                         shared_ptr<Sort> sort,
+                                                         vector<shared_ptr<Attribute>> &attributes) {
+    setConstant(constant);
+    setSort(sort);
+
+    for(vector<shared_ptr<Attribute>>::iterator it = attributes.begin(); it != attributes.end(); it++) {
+        this->attributes.push_back(*it);
+    }
+}
+
+shared_ptr<MetaSpecConstant> MetaSpecConstFunDeclaration::getConstant() {
+    return constant;
+}
+
+void MetaSpecConstFunDeclaration::setConstant(shared_ptr<MetaSpecConstant> constant) {
+    this->constant = constant;
+}
+
+shared_ptr<Sort> MetaSpecConstFunDeclaration::getSort() {
+    return sort;
+}
+
+void MetaSpecConstFunDeclaration::setSort(shared_ptr<Sort> sort) {
+    this->sort = sort;
+}
+
+vector<shared_ptr<Attribute>> &MetaSpecConstFunDeclaration::getAttributes() {
+    return attributes;
+}
+
+/* ============================== IdentifFunDeclaration =============================== */
+
+IdentifFunDeclaration::IdentifFunDeclaration(shared_ptr<IIdentifier> identifier,
+                                             vector<shared_ptr<Sort>> &signature) {
+    setIdentifier(identifier);
+
+    for(vector<shared_ptr<Sort>>::iterator it = signature.begin(); it != signature.end(); it++) {
+        this->signature.push_back(*it);
+    }
+}
+
+IdentifFunDeclaration::IdentifFunDeclaration(shared_ptr<IIdentifier> identifier,
+                                             vector<shared_ptr<Sort>> &signature,
+                                             vector<shared_ptr<Attribute>> &attributes) {
+    setIdentifier(identifier);
+
+    for(vector<shared_ptr<Sort>>::iterator it = signature.begin(); it != signature.end(); it++) {
+        this->signature.push_back(*it);
+    }
+
+    for(vector<shared_ptr<Attribute>>::iterator it = attributes.begin(); it != attributes.end(); it++) {
+        this->attributes.push_back(*it);
+    }
+}
+
+shared_ptr<IIdentifier> IdentifFunDeclaration::getIdentifier() {
+    return identifier;
+}
+
+void IdentifFunDeclaration::setIdentifier(shared_ptr<IIdentifier> identifier) {
+    this->identifier = identifier;
+}
+
+vector<shared_ptr<Sort>> &IdentifFunDeclaration::getSignature() {
+    return signature;
+}
+
+vector<shared_ptr<Attribute>> &IdentifFunDeclaration::getAttributes() {
+    return attributes;
+}
+
+/* =============================== ParamFunDeclaration ================================ */
+
+ParamFunDeclaration::ParamFunDeclaration(vector<shared_ptr<Symbol>> &params,
+                                         shared_ptr<IIdentifier> identifier,
+                                         vector<shared_ptr<Sort>> &signature) {
+    for(vector<shared_ptr<Symbol>>::iterator it = params.begin(); it != params.end(); it++) {
+        this->params.push_back(*it);
+    }
+
+    setIdentifier(identifier);
+
+    for(vector<shared_ptr<Sort>>::iterator it = signature.begin(); it != signature.end(); it++) {
+        this->signature.push_back(*it);
+    }
+}
+
+ParamFunDeclaration::ParamFunDeclaration(vector<shared_ptr<Symbol>> &params,
+                                         shared_ptr<IIdentifier> identifier,
+                                         vector<shared_ptr<Sort>> &signature,
+                                         vector<shared_ptr<Attribute>> &attributes) {
+    for(vector<shared_ptr<Symbol>>::iterator it = params.begin(); it != params.end(); it++) {
+        this->params.push_back(*it);
+    }
+
+    setIdentifier(identifier);
+
+    for(vector<shared_ptr<Sort>>::iterator it = signature.begin(); it != signature.end(); it++) {
+        this->signature.push_back(*it);
+    }
+
+    for(vector<shared_ptr<Attribute>>::iterator it = attributes.begin(); it != attributes.end(); it++) {
+        this->attributes.push_back(*it);
+    }
+}
+
+vector<shared_ptr<Symbol>> &ParamFunDeclaration::getParams() {
+    return params;
 }
--- ast/smt/smt_symbol_decl.h
+++ ast/smt/smt_symbol_decl.h
@@ -1,6 +1,6 @@
-//
-// Created by cristinaserban on 17.04.2015.
-//
+/** \file smt_symbol_decl.h
+ *  \brief Definition of classes representing sort and function symbol declarations.
+ */
 
 #ifndef PARSE_SMTLIB_SMT_SYMBOL_DECL_H
 #define PARSE_SMTLIB_SMT_SYMBOL_DECL_H
@@ -14,6 +14,13 @@
 #include "smt_sort.h"
 
 namespace smt {
+    /* =============================== SortSymbolDeclaration ============================== */
+
+    /**
+     * Declaration of a sort symbol.
+     * Node of the SMT abstract syntax tree.
+     * Can act as an attribute value.
+     */
     class SortSymbolDeclaration : public virtual SmtAstNode,
                                   public IAttributeValue {
     private:
@@ -21,14 +28,25 @@ namespace smt {
         long cardinality;
         std::vector<std::shared_ptr<Attribute>> attributes;
     public:
+        /**
+         * Constructor without attributes
+         * \param identifier    Sort symbol identiier
+         * \param cardinality   Sort cardinality
+         */
         SortSymbolDeclaration(std::shared_ptr<IIdentifier> identifier,
                               long cardinality);
 
+        /**
+         * Constructor with attributes
+         * \param identifier    Sort symbol identiier
+         * \param cardinality   Sort cardinality
+         * \param attributes    Sort symbol declaration attributes
+         */
         SortSymbolDeclaration(std::shared_ptr<IIdentifier> identifier,
                               long cardinality,
                               std::vector<std::shared_ptr<Attribute>> &attributes);
 
-        std::shared_ptr<IIdentifier> &getIdentifier();
+        std::shared_ptr<IIdentifier> getIdentifier();
         void setIdentifier(std::shared_ptr<IIdentifier> identifier);
 
         long getCardinality();
@@ -37,31 +55,64 @@ namespace smt {
         std::vector<std::shared_ptr<Attribute>> &getAttributes();
     };
 
+    /* =============================== FunSymbolDeclaration =============================== */
+
+    /**
+     * Abstract class for a function symbol declaration.
+     * Node of the SMT abstract syntax tree.
+     * Can act as an attribute value.
+     */
     class FunSymbolDeclaration : public virtual SmtAstNode,
                                  public IAttributeValue {  };
 
-    class SpecConstFunDeclaration : FunSymbolDeclaration {
+    /* ============================= SpecConstFunDeclaration ============================== */
+
+    /**
+     * Specification constant function symbol declaration.
+     * Node of the SMT abstract syntax tree.
+     * Can act as an attribute value.
+     */
+    class SpecConstFunDeclaration : public FunSymbolDeclaration {
     private:
         std::shared_ptr<ISpecConstant> constant;
         std::shared_ptr<Sort> sort;
         std::vector<std::shared_ptr<Attribute>> attributes;
+
     public:
+        /**
+        * Constructor without attributes
+        * \param constant      Specification constant
+        * \param sort          Function sort
+        */
         SpecConstFunDeclaration(std::shared_ptr<ISpecConstant> constant,
                                 std::shared_ptr<Sort> sort);
 
+        /**
+         * Constructor with attributes
+         * \param constant      Specification constant
+         * \param sort          Function sort
+         * \param attributes    Function symbol declaration attributes
+         */
         SpecConstFunDeclaration(std::shared_ptr<ISpecConstant> constant,
                                 std::shared_ptr<Sort> sort,
                                 std::vector<std::shared_ptr<Attribute>> &attributes);
 
-        std::shared_ptr<ISpecConstant> &getConstant();
+        std::shared_ptr<ISpecConstant> getConstant();
         void setConstant(std::shared_ptr<ISpecConstant> constant);
 
-        std::shared_ptr<Sort> &getSort();
+        std::shared_ptr<Sort> getSort();
         void setSort(std::shared_ptr<Sort> sort);
 
         std::vector<std::shared_ptr<Attribute>> &getAttributes();
     };
 
+    /* ========================== MetaSpecConstFunDeclaration =========================== */
+
+    /**
+     * Meta specification constant function symbol declaration.
+     * Node of the SMT abstract syntax tree.
+     * Can act as an attribute value.
+     */
     class MetaSpecConstFunDeclaration : public FunSymbolDeclaration {
     private:
         std::shared_ptr<MetaSpecConstant> constant;
@@ -69,40 +120,68 @@ namespace smt {
         std::vector<std::shared_ptr<Attribute>> attributes;
 
     public:
-
+        /**
+        * Constructor without attributes
+        * \param constant      Meta specification constant
+        * \param sort          Function sort
+        */
         MetaSpecConstFunDeclaration(std::shared_ptr<MetaSpecConstant> constant,
                                     std::shared_ptr<Sort> sort);
 
-        MetaSpecConstFunDeclaration(std::shared_ptr<IIdentifier> identifier,
+        /**
+         * Constructor with attributes
+         * \param constant      Meta specification constant
+         * \param sort          Function sort
+         * \param attributes    Function symbol declaration attributes
+         */
+        MetaSpecConstFunDeclaration(std::shared_ptr<MetaSpecConstant> constant,
                                     std::shared_ptr<Sort> sort,
                                     std::vector<std::shared_ptr<Attribute>> &attributes);
 
-        std::shared_ptr<MetaSpecConstant> &getConstant();
+        std::shared_ptr<MetaSpecConstant> getConstant();
         void setConstant(std::shared_ptr<MetaSpecConstant> constant);
 
-        std::shared_ptr<Sort> &getSort();
+        std::shared_ptr<Sort> getSort();
         void setSort(std::shared_ptr<Sort> sort);
 
         std::vector<std::shared_ptr<Attribute>> &getAttributes();
     };
 
+    /* ============================== IdentifFunDeclaration =============================== */
+
+    /**
+     * Identifier function symbol declaration.
+     * Node of the SMT abstract syntax tree.
+     * Can act as an attribute value.
+     */
     class IdentifFunDeclaration : public FunSymbolDeclaration {
     protected:
         std::shared_ptr<IIdentifier> identifier;
         std::vector<std::shared_ptr<Sort>> signature;
         std::vector<std::shared_ptr<Attribute>> attributes;
 
-        IdentifFunDeclaration();
+        IdentifFunDeclaration() { }
 
     public:
+        /**
+         * Constructor without attributes
+         * \param identifier    Function identifier
+         * \param signature     Function signature
+         */
         IdentifFunDeclaration(std::shared_ptr<IIdentifier> identifier,
                               std::vector<std::shared_ptr<Sort>> &signature);
 
+        /**
+         * Constructor with attributes
+         * \param identifier    Function identifier
+         * \param signature     Function signature
+         * \param attributes    Function symbol declaration attributes
+         */
         IdentifFunDeclaration(std::shared_ptr<IIdentifier> identifier,
                               std::vector<std::shared_ptr<Sort>> &signature,
                               std::vector<std::shared_ptr<Attribute>> &attributes);
 
-        std::shared_ptr<IIdentifier> &getIdentifier();
+        std::shared_ptr<IIdentifier> getIdentifier();
         void setIdentifier(std::shared_ptr<IIdentifier> identifier);
 
         std::vector<std::shared_ptr<Sort>> &getSignature();
@@ -110,15 +189,35 @@ namespace smt {
         std::vector<std::shared_ptr<Attribute>> &getAttributes();
     };
 
+    /* =============================== ParamFunDeclaration ================================ */
+
+    /**
+    * Parametric function symbol declaration.
+    * Node of the SMT abstract syntax tree.
+    * Can act as an attribute value.
+    */
     class ParamFunDeclaration : public IdentifFunDeclaration {
     private:
         std::vector<std::shared_ptr<Symbol>> params;
 
     public:
+        /**
+         * Constructor without attributes
+         * \param params        Function parameters
+         * \param identifier    Function identifier
+         * \param signature     Function signature
+         */
         ParamFunDeclaration(std::vector<std::shared_ptr<Symbol>> &params,
                             std::shared_ptr<IIdentifier> identifier,
                             std::vector<std::shared_ptr<Sort>> &signature);
 
+        /**
+         * Constructor with attributes
+         * \param params        Function parameters
+         * \param identifier    Function identifier
+         * \param signature     Function signature
+         * \param attributes    Function symbol declaration attributes
+         */
         ParamFunDeclaration(std::vector<std::shared_ptr<Symbol>> &params,
                             std::shared_ptr<IIdentifier> identifier,
                             std::vector<std::shared_ptr<Sort>> &signature,
