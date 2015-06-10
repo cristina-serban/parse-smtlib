fded760638c2db587038cb477a7666cbfc24e6f9
Cristina Serban <cristina.serban89@gmail.com>
2015-06-11 00:51:27 +0200

    Structure for syntax check

---
+++ smtlib/visitor/ast_syntax_checker.cpp
@@ -0,0 +1,242 @@
+#include <visitor/ast_syntax_checker.h>
+#include <ast/ast_attribute.h>
+
+using namespace smtlib::ast;
+
+void AstSyntaxChecker::visit(Attribute const *node) {
+
+}
+
+void AstSyntaxChecker::visit(CompoundAttributeValue const *node) {
+
+}
+
+void AstSyntaxChecker::visit(Symbol const *node) {
+
+}
+
+void AstSyntaxChecker::visit(Keyword const *node) {
+
+}
+
+void AstSyntaxChecker::visit(MetaSpecConstant const *node) {
+
+}
+
+void AstSyntaxChecker::visit(BooleanValue const *node) {
+
+}
+
+void AstSyntaxChecker::visit(PropLiteral const *node) {
+
+}
+
+void AstSyntaxChecker::visit(AssertCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(CheckSatCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(CheckSatAssumCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(DeclareConstCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(DeclareFunCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(DeclareSortCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(DefineFunCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(DefineFunRecCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(DefineFunsRecCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(DefineSortCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(EchoCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(ExitCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(GetAssertsCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(GetAssignsCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(GetInfoCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(GetModelCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(GetOptionCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(GetProofCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(GetUnsatAssumsCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(GetUnsatCoreCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(GetValueCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(PopCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(PushCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(ResetCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(ResetAssertsCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(SetInfoCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(SetLogicCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(SetOptionCommand const *node) {
+
+}
+
+void AstSyntaxChecker::visit(FunctionDeclaration const *node) {
+
+}
+
+void AstSyntaxChecker::visit(FunctionDefinition const *node) {
+
+}
+
+void AstSyntaxChecker::visit(Identifier const *node) {
+
+}
+
+void AstSyntaxChecker::visit(QualifiedIdentifier const *node) {
+
+}
+
+void AstSyntaxChecker::visit(DecimalLiteral const *node) {
+
+}
+
+void AstSyntaxChecker::visit(NumeralLiteral const *node) {
+
+}
+
+void AstSyntaxChecker::visit(StringLiteral const *node) {
+
+}
+
+void AstSyntaxChecker::visit(Logic const *node) {
+
+}
+
+void AstSyntaxChecker::visit(Theory const *node) {
+
+}
+
+void AstSyntaxChecker::visit(Script const *node) {
+
+}
+
+
+void AstSyntaxChecker::visit(Sort const *node) {
+
+}
+
+void AstSyntaxChecker::visit(CompSExpression const *node) {
+
+}
+
+void AstSyntaxChecker::visit(SortSymbolDeclaration const *node) {
+
+}
+
+void AstSyntaxChecker::visit(SpecConstFunDeclaration const *node) {
+
+}
+
+void AstSyntaxChecker::visit(MetaSpecConstFunDeclaration const *node) {
+
+}
+
+void AstSyntaxChecker::visit(IdentifierFunDeclaration const *node) {
+
+}
+
+void AstSyntaxChecker::visit(ParametricFunDeclaration const *node) {
+
+}
+
+void AstSyntaxChecker::visit(QualifiedTerm const *node) {
+
+}
+
+void AstSyntaxChecker::visit(LetTerm const *node) {
+
+}
+
+void AstSyntaxChecker::visit(ForallTerm const *node) {
+
+}
+
+void AstSyntaxChecker::visit(ExistsTerm const *node) {
+
+}
+
+void AstSyntaxChecker::visit(AnnotatedTerm const *node) {
+
+}
+
+void AstSyntaxChecker::visit(SortedVariable const *node) {
+
+}
+
+void AstSyntaxChecker::visit(VarBinding const *node) {
+
+}
+
---
+++ smtlib/visitor/ast_syntax_checker.h
@@ -0,0 +1,96 @@
+#include "ast_visitor.h"
+
+#ifndef PARSE_SMTLIB_AST_SYNTAX_CHECKER_H
+#define PARSE_SMTLIB_AST_SYNTAX_CHECKER_H
+
+#include <string>
+#include <vector>
+
+namespace smtlib {
+    namespace ast {
+        struct SyntaxCheckError {
+            std::string message;
+            AstNode const *node;
+        };
+
+        class AstSyntaxChecker : public AstVisitor1<bool> {
+        private:
+            std::vector<SyntaxCheckError> errors;
+        public:
+
+            virtual void visit(Attribute const *node);
+            virtual void visit(CompoundAttributeValue const *node);
+
+            virtual void visit(Symbol const *node);
+            virtual void visit(Keyword const *node);
+            virtual void visit(MetaSpecConstant const *node);
+            virtual void visit(BooleanValue const *node);
+            virtual void visit(PropLiteral const *node);
+
+            virtual void visit(AssertCommand const *node);
+            virtual void visit(CheckSatCommand const *node);
+            virtual void visit(CheckSatAssumCommand const *node);
+            virtual void visit(DeclareConstCommand const *node);
+            virtual void visit(DeclareFunCommand const *node);
+            virtual void visit(DeclareSortCommand const *node);
+            virtual void visit(DefineFunCommand const *node);
+            virtual void visit(DefineFunRecCommand const *node);
+            virtual void visit(DefineFunsRecCommand const *node);
+            virtual void visit(DefineSortCommand const *node);
+            virtual void visit(EchoCommand const *node);
+            virtual void visit(ExitCommand const *node);
+            virtual void visit(GetAssertsCommand const *node);
+            virtual void visit(GetAssignsCommand const *node);
+            virtual void visit(GetInfoCommand const *node);
+            virtual void visit(GetModelCommand const *node);
+            virtual void visit(GetOptionCommand const *node);
+            virtual void visit(GetProofCommand const *node);
+            virtual void visit(GetUnsatAssumsCommand const *node);
+            virtual void visit(GetUnsatCoreCommand const *node);
+            virtual void visit(GetValueCommand const *node);
+            virtual void visit(PopCommand const *node);
+            virtual void visit(PushCommand const *node);
+            virtual void visit(ResetCommand const *node);
+            virtual void visit(ResetAssertsCommand const *node);
+            virtual void visit(SetInfoCommand const *node);
+            virtual void visit(SetLogicCommand const *node);
+            virtual void visit(SetOptionCommand const *node);
+
+            virtual void visit(FunctionDeclaration const *node);
+            virtual void visit(FunctionDefinition const *node);
+
+            virtual void visit(Identifier const *node);
+            virtual void visit(QualifiedIdentifier const *node);
+
+            virtual void visit(DecimalLiteral const *node);
+            virtual void visit(NumeralLiteral const *node);
+            virtual void visit(StringLiteral const *node);
+
+            virtual void visit(Logic const *node);
+            virtual void visit(Theory const *node);
+            virtual void visit(Script const *node);
+
+            virtual void visit(Sort const *node);
+
+            virtual void visit(CompSExpression const *node);
+
+            virtual void visit(SortSymbolDeclaration const *node);
+
+            virtual void visit(SpecConstFunDeclaration const *node);
+            virtual void visit(MetaSpecConstFunDeclaration const *node);
+            virtual void visit(IdentifierFunDeclaration const *node);
+            virtual void visit(ParametricFunDeclaration const *node);
+
+            virtual void visit(QualifiedTerm const *node);
+            virtual void visit(LetTerm const *node);
+            virtual void visit(ForallTerm const *node);
+            virtual void visit(ExistsTerm const *node);
+            virtual void visit(AnnotatedTerm const *node);
+
+            virtual void visit(SortedVariable const *node);
+            virtual void visit(VarBinding const *node);
+        };
+    }
+}
+
+#endif //PARSE_SMTLIB_AST_SYNTAX_CHECKER_H
